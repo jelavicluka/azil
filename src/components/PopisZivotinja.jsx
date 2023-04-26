7a872f80da6762bfe4f5b036b09554eaa42f0228
Luka Jelavić Šako <lukajelavi@gmail.com>
2023-04-26 20:03:16 +0200

    Added type and availability filetrs and filter styling

--- src/components/PopisZivotinja.jsx
+++ src/components/PopisZivotinja.jsx
@@ -2,19 +2,198 @@ import AnimalCard from "./AnimalCard";
 import axios from "axios";
 import "../App.css";
 import { Reveal } from "react-reveal";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Box } from "@mui/system";
 
 function PopisZivotinja({ zivotinje, postaviZivotinje }) {
-  async function filtriraj(e) {
-    const podatci = await axios
-      .get(`http://localhost:3001/zivotinje/?vrsta=${e.target.value}`)
-      .then((zivotinje) => postaviZivotinje(zivotinje.data));
+  async function filtrirajVrste(e) {
+    e.target.value === ""
+      ? await axios
+          .get(`http://localhost:3001/zivotinje`)
+          .then((zivotinje) => postaviZivotinje(zivotinje.data))
+      : await axios
+          .get(`http://localhost:3001/zivotinje/?vrsta=${e.target.value}`)
+          .then((zivotinje) => postaviZivotinje(zivotinje.data));
+  }
+  async function filtrirajDostupnost(e) {
+    e.target.value === ""
+      ? await axios
+          .get(`http://localhost:3001/zivotinje`)
+          .then((zivotinje) => postaviZivotinje(zivotinje.data))
+      : await axios
+          .get(`http://localhost:3001/zivotinje/?udomljen=${e.target.value}`)
+          .then((zivotinje) => postaviZivotinje(zivotinje.data));
   }
 
   return (
     <div>
       <Reveal>
         <h1>Popis Životinja</h1>
-        <h1>Filteri</h1>
+        <h2 className="filter-heading">Filteri</h2>
+        <div className="all-filters-container">
+          <div>
+            <div className="inputs-container">
+              <div className="input-heading">Vrsta:</div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Sve
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={""}
+                      onChange={filtrirajVrste}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Pas
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={"pas"}
+                      onChange={filtrirajVrste}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Mačka
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={"mačka"}
+                      onChange={filtrirajVrste}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+            </div>
+          </div>
+          <div className="filter-container">
+            <div className="inputs-container">
+              <div className="input-heading">Dostupnost:</div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Sve
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={""}
+                      onChange={filtrirajDostupnost}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Udomljen
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={"true"}
+                      onChange={filtrirajDostupnost}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Nije udomljen
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={"false"}
+                      onChange={filtrirajDostupnost}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="card-container">
           {zivotinje.map((r) => (
             <div className="card">
