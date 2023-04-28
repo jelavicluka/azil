573a447cc8d2a1aa79c9e596d92300622bb4b1cf
Luka Jelavić Šako <lukajelavi@gmail.com>
2023-04-28 14:29:09 +0200

    Completed filters styling

--- src/components/PopisZivotinja.jsx
+++ src/components/PopisZivotinja.jsx
@@ -30,172 +30,177 @@ function PopisZivotinja({ zivotinje, postaviZivotinje }) {
     <div>
       <Reveal>
         <h1>Popis Životinja</h1>
-        <h2 className="filter-heading">Filteri</h2>
-        <div>
-          <div className="inputs-container">
-            <div className="input-heading">Vrsta:</div>
-            <div className="input-container">
-              <FormControlLabel
-                style={{ color: "white" }}
-                htmlFor="filter"
-                label={
-                  <Box component="div" fontSize={14}>
-                    Sve
-                  </Box>
-                }
-                control={
-                  <Checkbox
-                    name="filter"
-                    type="checkbox"
-                    value={""}
-                    onChange={filtrirajVrste}
-                    sx={{
-                      color: "#ffffff",
-                      "&.Mui-checked": {
-                        color: "rgb(92, 170, 113)",
-                      },
-                    }}
+        <div className="popis-grid-container">
+          <div className="item-filters">
+            <h2 className="filter-heading">Filteri</h2>
+            <div>
+              <div className="inputs-container">
+                <div className="input-heading">Vrsta:</div>
+                <div className="input-container">
+                  <FormControlLabel
+                    style={{ color: "white" }}
+                    htmlFor="filter"
+                    label={
+                      <Box component="div" fontSize={14}>
+                        Sve
+                      </Box>
+                    }
+                    control={
+                      <Checkbox
+                        name="filter"
+                        type="checkbox"
+                        value={""}
+                        onChange={filtrirajVrste}
+                        sx={{
+                          color: "#ffffff",
+                          "&.Mui-checked": {
+                            color: "rgb(92, 170, 113)",
+                          },
+                        }}
+                      />
+                    }
                   />
-                }
-              />
-            </div>
-            <div className="input-container">
-              <FormControlLabel
-                style={{ color: "white" }}
-                htmlFor="filter"
-                label={
-                  <Box component="div" fontSize={14}>
-                    Pas
-                  </Box>
-                }
-                control={
-                  <Checkbox
-                    name="filter"
-                    type="checkbox"
-                    value={"pas"}
-                    onChange={filtrirajVrste}
-                    sx={{
-                      color: "#ffffff",
-                      "&.Mui-checked": {
-                        color: "rgb(92, 170, 113)",
-                      },
-                    }}
+                </div>
+                <div className="input-container">
+                  <FormControlLabel
+                    style={{ color: "white" }}
+                    htmlFor="filter"
+                    label={
+                      <Box component="div" fontSize={14}>
+                        Pas
+                      </Box>
+                    }
+                    control={
+                      <Checkbox
+                        name="filter"
+                        type="checkbox"
+                        value={"pas"}
+                        onChange={filtrirajVrste}
+                        sx={{
+                          color: "#ffffff",
+                          "&.Mui-checked": {
+                            color: "rgb(92, 170, 113)",
+                          },
+                        }}
+                      />
+                    }
                   />
-                }
-              />
-            </div>
-            <div className="input-container">
-              <FormControlLabel
-                style={{ color: "white" }}
-                htmlFor="filter"
-                label={
-                  <Box component="div" fontSize={14}>
-                    Mačka
-                  </Box>
-                }
-                control={
-                  <Checkbox
-                    name="filter"
-                    type="checkbox"
-                    value={"mačka"}
-                    onChange={filtrirajVrste}
-                    sx={{
-                      color: "#ffffff",
-                      "&.Mui-checked": {
-                        color: "rgb(92, 170, 113)",
-                      },
-                    }}
+                </div>
+                <div className="input-container">
+                  <FormControlLabel
+                    style={{ color: "white" }}
+                    htmlFor="filter"
+                    label={
+                      <Box component="div" fontSize={14}>
+                        Mačka
+                      </Box>
+                    }
+                    control={
+                      <Checkbox
+                        name="filter"
+                        type="checkbox"
+                        value={"mačka"}
+                        onChange={filtrirajVrste}
+                        sx={{
+                          color: "#ffffff",
+                          "&.Mui-checked": {
+                            color: "rgb(92, 170, 113)",
+                          },
+                        }}
+                      />
+                    }
                   />
-                }
-              />
+                </div>
+              </div>
+            </div>
+            <div className="inputs-container">
+              <div className="input-heading">Dostupnost:</div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Sve
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={""}
+                      onChange={filtrirajDostupnost}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Udomljen
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={"true"}
+                      onChange={filtrirajDostupnost}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
+              <div className="input-container">
+                <FormControlLabel
+                  style={{ color: "white" }}
+                  htmlFor="filter"
+                  label={
+                    <Box component="div" fontSize={14}>
+                      Nije udomljen
+                    </Box>
+                  }
+                  control={
+                    <Checkbox
+                      name="filter"
+                      type="checkbox"
+                      value={"false"}
+                      onChange={filtrirajDostupnost}
+                      sx={{
+                        color: "#ffffff",
+                        "&.Mui-checked": {
+                          color: "rgb(92, 170, 113)",
+                        },
+                      }}
+                    />
+                  }
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="inputs-container">
-          <div className="input-heading">Dostupnost:</div>
-          <div className="input-container">
-            <FormControlLabel
-              style={{ color: "white" }}
-              htmlFor="filter"
-              label={
-                <Box component="div" fontSize={14}>
-                  Sve
-                </Box>
-              }
-              control={
-                <Checkbox
-                  name="filter"
-                  type="checkbox"
-                  value={""}
-                  onChange={filtrirajDostupnost}
-                  sx={{
-                    color: "#ffffff",
-                    "&.Mui-checked": {
-                      color: "rgb(92, 170, 113)",
-                    },
-                  }}
-                />
-              }
-            />
-          </div>
-          <div className="input-container">
-            <FormControlLabel
-              style={{ color: "white" }}
-              htmlFor="filter"
-              label={
-                <Box component="div" fontSize={14}>
-                  Udomljen
-                </Box>
-              }
-              control={
-                <Checkbox
-                  name="filter"
-                  type="checkbox"
-                  value={"true"}
-                  onChange={filtrirajDostupnost}
-                  sx={{
-                    color: "#ffffff",
-                    "&.Mui-checked": {
-                      color: "rgb(92, 170, 113)",
-                    },
-                  }}
-                />
-              }
-            />
-          </div>
-          <div className="input-container">
-            <FormControlLabel
-              style={{ color: "white" }}
-              htmlFor="filter"
-              label={
-                <Box component="div" fontSize={14}>
-                  Nije udomljen
-                </Box>
-              }
-              control={
-                <Checkbox
-                  name="filter"
-                  type="checkbox"
-                  value={"false"}
-                  onChange={filtrirajDostupnost}
-                  sx={{
-                    color: "#ffffff",
-                    "&.Mui-checked": {
-                      color: "rgb(92, 170, 113)",
-                    },
-                  }}
-                />
-              }
-            />
-          </div>
-        </div>
-
-        <div className="card-container">
-          {zivotinje.map((r) => (
-            <div className="card">
-              <AnimalCard key={r.id} zivotinja={r} />
+          <div className="item-cards">
+            <div className="card-container">
+              {zivotinje.map((r) => (
+                <div className="card">
+                  <AnimalCard key={r.id} zivotinja={r} />
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </Reveal>
     </div>
